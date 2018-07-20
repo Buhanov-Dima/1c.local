e14403961d0bd20d0f5e583da7e5a10d8da2a5d0
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-20 14:21:50 +0500

    &

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>1c</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
+	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="css/style-xxl.css" media="only screen and (max-width:1450px)">
+	<link rel="stylesheet" href="css/style-lg.css" media="only screen and (max-width:1200px)">
+	<link rel="stylesheet" href="css/style-xl.css" media="only screen and (max-width:992px)">
+	<link rel="stylesheet" href="css/style-md.css" media="only screen and (max-width:768px)">
+	<link rel="stylesheet" href="css/style-xs.css" media="only screen and (max-width:576px)">
+</head>
+<body>
+<header>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="fx-cnt-between">
+	  <a class="navbar-brand logo" href="#">
+	  	<img src="/img/icons/logo.png" alt="logo">
+	  </a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="#">active</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">active</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">active</a>
+	      </li>
+	      <li class="nav-item">
+	    	<a class="nav-link" href="#">active</a>
+	      </li>
+	    </ul>
+	  </div>
+	  <div class="lang">
+	  	<ul>
+	  	  <li class="nav-item dropdown">
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="#">Action</a>
+	          <a class="dropdown-item" href="#">Another action</a>
+	          <div class="dropdown-divider"></div>
+	          <a class="dropdown-item" href="#">Something else here</a>
+	        </div>
+	      </li>
+	    </ul>
+	   </div>
+	  </div>
+	</nav>
+</header>
