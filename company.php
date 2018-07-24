d33470fa2edd3189e43b4f2c23af3ec8b5357bdc
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-24 15:13:15 +0500

    company

---
+++ company.php
@@ -0,0 +1,22 @@
+<?php include ('header-white.php'); ?>
+
+<section class="wrap-company">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="company">
+					<h3 class="standart-h-wh">О КОМПАНИИ</h3>
+					<p>Вы пользуетесь облачной 1С в любое время, из любой части мира, <br>а мы консультируем, дорабатываем 1С и развиваем наш сервис</p>
+					<div class="comp-menu">
+						<a href="#">Новости</a>
+						<a href="#">Преимущества</a>
+						<a href="#">Команда и дата-центр</a>
+						<a href="#">Юридическая информация</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include ('footer-white.php'); ?>
