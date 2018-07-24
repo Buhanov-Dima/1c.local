b18d1bdab388889e5bfa4964d09c4f7f3477277d
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-24 14:11:53 +0500

    team + news

---
+++ news.php
@@ -0,0 +1,68 @@
+<?php include ('header-white.php'); ?>
+
+<section class="news-b1">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="wrap-team">
+					<h3 class="standart-h-wh">Новости</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="wrap-news">
+					<div class="news">
+						<div class="news-txt">
+							<h4>Lorem</h4>
+							<span>21.10.18</span>
+							<div class="news-txt-main">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia enim culpa, odio dignissimos ab commodi eos sunt at, voluptates quos illo nobis architecto amet nesciunt. Tempore voluptates architecto cumque illo!</p>
+							</div>
+							<div class="text-right"><button class="btn-news">Подробнее</button></div>
+						</div>
+
+						<div class="news-img">
+							<img src="/img/main/test.png" alt="">
+						</div>
+					</div>
+
+					<div class="news">
+						<div class="news-txt">
+							<h4>Lorem</h4>
+							<span>21.10.18</span>
+							<div class="news-txt-main">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia enim culpa, odio dignissimos ab commodi eos sunt at, voluptates quos illo nobis architecto amet nesciunt. Tempore voluptates architecto cumque illo!</p>
+							</div>
+							<div class="text-right"><button class="btn-news">Подробнее</button></div>
+						</div>
+
+						<div class="news-img">
+							<img src="/img/main/test.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="team-plan">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="team-plans">
+					<a href="#">ОЗНАКОМИТЬСЯ С ТАРИФНЫМИ ПЛАНАМИ</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include ('footer-white.php'); ?>
---
+++ team.php
@@ -0,0 +1,86 @@
+<?php include ('header-white.php'); ?>
+
+<section class="team-b1">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="wrap-team">
+					<h3 class="standart-h-wh">Команда и дата-центр</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="teams">
+						<div class="team-item">
+							<div class="item-img"><img src="/img/main/team-1.png" alt=""></div>
+							<div class="item-txt">
+								<p>Марина Ивановна Шпиц</p>
+								<p>Должность</p>
+								<p>Стаж</p>
+							</div>
+						</div>
+						<div class="team-item">
+							<div class="item-img"><img src="/img/main/team-2.png" alt=""></div>
+							<div class="item-txt">
+								<p>Василий Васильевич Иванов</p>
+								<p>Должность</p>
+								<p>Стаж</p>
+							</div>
+						</div>
+						<div class="team-item">
+							<div class="item-img"><img src="/img/main/team-3.png" alt=""></div>
+							<div class="item-txt">
+								<p>Иван Васильевич Иванов</p>
+								<p>Должность</p>
+								<p>Стаж</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class="team-b3">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="data-center text-center">
+					<p class="data-txt">Дата-центр:</p>
+					<p>Россия, Москва, дата-центр DataPro</p>
+					<p class="data-txt">Офисы компании «Сервис Клауд»:</p>
+					<p><b>Юридический адрес:</b></p>
+					<p>г. Москва, пр-т Вернадского, д. 29<br> +7 495 786-59-15</p>
+				</div>
+				<div class="maps">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.417336929432!2d37.5142051160579!3d55.68173170493849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54d00081f680f%3A0xe1d097774fd80c86!2z0L_RgC4g0JLQtdGA0L3QsNC00YHQutC-0LPQviwgMjksINCc0L7RgdC60LLQsCwgMTE5MzMx!5e0!3m2!1sru!2sru!4v1532420450893" width="100%" height="510" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+				<div class="data-center text-center">
+					<p class="data-txt2">г. Тула, ул. Болдина, д. 98<br>ИНН 7104516184/КПП 710401001<br>8 800 555-91-15 (звонок бесплатный по всей России)<br>Электронная почта help@scloud.ru</p>
+				</div>
+				<div class="maps">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2335.4358134816725!2d37.57160031601431!3d54.17235712021873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41344007ce4db575%3A0x93a09d629f00ca87!2z0YPQuy4g0JHQvtC70LTQuNC90LAsIDk4LCDQotGD0LvQsCwg0KLRg9C70YzRgdC60LDRjyDQvtCx0LsuLCAzMDAwMjg!5e0!3m2!1sru!2sru!4v1532420839598" width="100%" height="510" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="team-plan">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<div class="team-plans">
+					<a href="#">ОЗНАКОМИТЬСЯ С ТАРИФНЫМИ ПЛАНАМИ</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include ('footer-white.php'); ?>
