04bd056b6f532ae242f4c9b2fd24b92d4950c73d
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-08-07 20:39:08 +0500

    1

--- footer-white.php
+++ footer-white.php
@@ -12,12 +12,36 @@
 		</div>
 	</div>
 </footer>
+
+
+        <div style="display: none;" id="hidden-form">
+         <div class="m-b2-form">
+			<form action="" class="f1-main popup">
+				<input type="text" name="" class="f1-name" placeholder="Ваше имя">
+				<input type="text" name="" class="f1-mail" placeholder="Email">
+				<input type="text" name="" class="f1-phone" placeholder="Телефон">
+				<button class="f1-btn">Отправить</button>
+				<p>Нажимая на кнопку "Отправить", я даю согласие на обработку моих персональных</p>
+			</form>
+		  </div>
+        </div>
+
+        <div style="display: none;" id="hidden-thanks">
+         <div class="thanks">
+			<h3>Спасибо за обращение</h3>
+			<p>Мы Вам перезвоним в течении 10 минут</p>
+		 </div>
+        </div>
+
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
 	<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 	<script src="/js/main.js"></script>
+
+
 
 </body>
 </html>
