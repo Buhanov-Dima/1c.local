b9c72df3d075c29f742c4828fd5a414d67cf3dc5
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-08-10 17:49:51 +0500

    maps

--- team.php
+++ team.php
@@ -51,27 +51,29 @@
 		<div class="row">
 			<div class="col-xl-12">
 				<div class="data-center text-center">
-					<div class="data-txt">
-						<p>Дата-центр:</p>
+					<div  class="data-txt2">
+						<p>г. Москва, Большая Татарская улица, д. 44</p>
 					</div>
-					<p>Россия, Москва, дата-центр DataPro</p>
-					<div class="data-txt">
-						<p>Офисы компании «Сервис Клауд»:</p>
-					</div>
-					<p><b>Юридический адрес:</b></p>
-					<p>г. Москва, пр-т Вернадского, д. 29<br> +7 495 786-59-15</p>
 				</div>
 				<div class="maps">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.417336929432!2d37.5142051160579!3d55.68173170493849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54d00081f680f%3A0xe1d097774fd80c86!2z0L_RgC4g0JLQtdGA0L3QsNC00YHQutC-0LPQviwgMjksINCc0L7RgdC60LLQsCwgMTE5MzMx!5e0!3m2!1sru!2sru!4v1532420450893" width="100%" height="510" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Afdba23f65ccff42a7bfd680d157c5d0011f46b8517d1b7842f7815b2dada75f6&amp;width=100%25&amp;height=510&amp;lang=ru_RU&amp;scroll=false"></script>
 				</div>
 				<div class="data-center text-center">
 					<div  class="data-txt2">
-						<p>г. Тула, ул. Болдина, д. 98<br>ИНН 7104516184/КПП 710401001<br>8 800 555-91-15 (звонок бесплатный по всей России)<br>Электронная почта help@scloud.ru</p>
+						<p>г. Санкт-Петербург, БЦ «Акватория», Выборгская наб., д.61, Лит. А</p>
 					</div>
 				</div>
 				<div class="maps">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2335.4358134816725!2d37.57160031601431!3d54.17235712021873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41344007ce4db575%3A0x93a09d629f00ca87!2z0YPQuy4g0JHQvtC70LTQuNC90LAsIDk4LCDQotGD0LvQsCwg0KLRg9C70YzRgdC60LDRjyDQvtCx0LsuLCAzMDAwMjg!5e0!3m2!1sru!2sru!4v1532420839598" width="100%" height="510" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A38f17133e7fcd5f322629e10f6f6ba582dd14b426a9be29717c3cd43abc12168&amp;width=100%25&amp;height=510&amp;lang=ru_RU&amp;scroll=false"></script>
 				</div>
+				<div class="data-center text-center">
+					<div  class="data-txt2">
+						<p>г. Пермь, Овчинникова д. 10</p>
+					</div>
+				</div>
+				<div class="maps">
+					<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A0f35c573b6c21a31bb7ef6d9aba4dd4f6216b4f8b66742405f47c0ae5ccbe563&amp;width=100%25&amp;height=510&amp;lang=ru_RU&amp;scroll=false"></script>
+				</div>				
 			</div>
 		</div>
 	</div>
